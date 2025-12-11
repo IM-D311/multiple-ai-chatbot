9b0fac1ce165633fc50abe6c218305da9b647e3c
Imran Mulla <imulla311@gmail.com>
2025-12-11 14:04:04 +0530

    Create health.js

---
+++ api/health.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  return res.status(200).json({
+    status: 'ok',
+    message: 'Server is running',
+    timestamp: new Date().toISOString()
+  });
+}
